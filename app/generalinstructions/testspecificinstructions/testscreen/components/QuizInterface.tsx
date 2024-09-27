ca36c9f0732218c82167aaa34618f987bcb7fab3
sivakkumaran44 <sivakkumaran.g@sangaas.com>
2024-09-27 17:00:13 +0530

    screen

--- app/generalinstructions/testspecificinstructions/testscreen/components/QuizInterface.tsx
+++ app/generalinstructions/testspecificinstructions/testscreen/components/QuizInterface.tsx
@@ -1,4 +1,3 @@
-"use client"
 import React, { useState, useEffect } from 'react';
 import quizData from '@/components/data/questionpaper.json';
 import TestQuestion from './TestQuestion';
--- app/generalinstructions/testspecificinstructions/testscreen/components/Testscreen.tsx
+++ app/generalinstructions/testspecificinstructions/testscreen/components/Testscreen.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useState } from 'react';
 import TestHeader from './Testheader';
 import QuizInterface from './QuizInterface';
