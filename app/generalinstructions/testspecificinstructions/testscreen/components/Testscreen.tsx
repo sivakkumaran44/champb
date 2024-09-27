4eb8791ba9d6270e3910da7bbe99257cee455598
sivakkumaran44 <sivakkumaran.g@sangaas.com>
2024-09-27 16:47:08 +0530

    screen

--- app/generalinstructions/testspecificinstructions/testscreen/components/Testscreen.tsx
+++ app/generalinstructions/testspecificinstructions/testscreen/components/Testscreen.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from 'react';
-import TestHeader from './TestHeader';
+import TestHeader from './Testheader';
 import QuizInterface from './QuizInterface';
 
 import questionsData from '@/components/data/questionpaper.json';
