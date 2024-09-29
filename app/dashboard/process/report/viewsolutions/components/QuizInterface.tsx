3fdda71129ffe19ef7270eae0dccd859c6b447d0
sivakkumaran44 <sivakkumaran.g@sangaas.com>
2024-09-29 16:28:59 +0530

    screen

--- app/dashboard/process/report/viewsolutions/components/QuizInterface.tsx
+++ app/dashboard/process/report/viewsolutions/components/QuizInterface.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import quizData from '@/components/data/questionpaper.json';
 import QuizHeader from './QuizHeader';
