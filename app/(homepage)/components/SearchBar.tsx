f03c8ab3998c0e0668fd82dc8b79533751597e9e
sivakkumaran44 <sivakkumaran.g@sangaas.com>
2024-10-03 17:08:05 +0530

    screen

--- app/(homepage)/components/SearchBar.tsx
+++ app/(homepage)/components/SearchBar.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 import React, { useRef, useEffect, useState } from 'react';
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
-import examsData from '@/components/data/exam.json';  
+import examsData from '@/components/data/exam.json';
 
 interface Exam {
   id: string;
@@ -13,11 +13,14 @@ interface Exam {
 const SearchBar: React.FC<{ isMobile?: boolean }> = ({ isMobile = false }) => {
   const searchRef = useRef<HTMLDivElement | null>(null);
   const [searchTerm, setSearchTerm] = useState('');
+  
+  // Convert exam data to the desired format
   const allExams: Exam[] = examsData.map(exam => ({
     ...exam,
     id: exam.id.toString(),
   }));
-  const filteredExams = allExams.filter((exam) => 
+
+  const filteredExams = allExams.filter(exam => 
     exam.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -36,23 +39,23 @@ const SearchBar: React.FC<{ isMobile?: boolean }> = ({ isMobile = false }) => {
 
   return (
     <div className={`relative flex-grow ${isMobile ? 'w-full' : 'md:w-[600px] w-full max-w-3xl mx-8 mt-2 md:mt-0'}`}>
-      <div className="flex items-center border-2 border-slate-700 rounded-full overflow-hidden shadow-[0_5px_0_0_0_#6EE7B7]">
+      <div className="relative ml-auto flex-1 md:grow-0">
         <div ref={searchRef} className="flex-grow relative">
+          <Search className="absolute left-2.5 top-3.5 h-4 w-4 text-muted-foreground" />
           <Input
             type="text"
             name="exam"
             placeholder="Search your exam"
+            className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="w-full px-4 py-2 bg-white text-slate-700 focus:outline-none border-none"
           />
-          <Search className="text-slate-500 absolute right-3 top-1/2 transform -translate-y-1/2" size={20} />
         </div>
       </div>
       {searchTerm && (
-        <div className="absolute z-10 w-full bg-white border border-slate-200 rounded-lg shadow-lg mt-1 max-h-60 overflow-y-auto">
+        <div className="absolute z-10 w-7/12 bg-white border border-slate-200 rounded-lg shadow-lg mt-1 max-h-60 overflow-y-auto">
           {filteredExams.length > 0 ? (
-            filteredExams.map((exam) => (
+            filteredExams.map(exam => (
               <div key={exam.id} className="p-3 hover:bg-slate-100 cursor-pointer last:border-b-0">
                 <div className="flex items-center justify-between text-sm text-slate-600">
                   <span>{exam.name}</span>
