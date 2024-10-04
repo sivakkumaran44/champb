2122e3a80d54a0caf577bdd1999953f8a4b40fc3
sivakkumaran44 <sivakkumaran.g@sangaas.com>
2024-10-04 16:05:17 +0530

    screen

--- app/dashboard/subscription/components/Banner.tsx
+++ app/dashboard/subscription/components/Banner.tsx
@@ -1,34 +1,36 @@
-import React from 'react';
-import { Button } from "@/components/ui/button";
-import SSCLogo from '@/public/assets/img/Banner.png'; 
-import Image from 'next/image';
-import { ArrowRight } from 'lucide-react';
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import SSCLogo from '@/public/assets/img/Banner.png'
+import Image from 'next/image'
+import { ArrowRight } from 'lucide-react'
 
 export default function Banner() {
   return (
-    <div className="flex flex-row justify-between items-center p-6 rounded-lg bg-gradient-to-b from-yellow-200/70 to-yellow-400/70 space-x-4">
-      <div className="flex flex-col space-y-4 text-left">
-        <p className="text-lg text-slate-700">Start Achieving your goal</p>
-        <h2 className="text-5xl font-bold text-slate-700">SSC CGL</h2>
-        <Button className="w-40 bg-emerald-500 hover:bg-emerald-600 flex items-center justify-center">
-          BUY NOW
-          <ArrowRight className="ml-2"/> 
-        </Button>
-        <ul className="list-disc list-inside text-sm text-slate-700 space-y-1">
+    <div className="flex flex-col-reverse sm:flex-row justify-between items-center p-4 sm:p-6 rounded-lg bg-gradient-to-b from-yellow-200/70 to-yellow-400/70 space-y-4 sm:space-y-0 sm:space-x-4">
+      <div className="flex flex-col space-y-2 sm:space-y-4 text-left w-full sm:w-auto">
+        <p className="text-base sm:text-lg text-slate-700">Start Achieving your goal</p>
+        <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold text-slate-700">SSC CGL</h2>
+        <ul className="list-disc list-inside text-xs sm:text-sm text-slate-700 space-y-1">
           <li>Full syllabus coverage for all SSC CGL Tiers (I, II, III, and IV)</li>
           <li>50+ full-length mock tests</li>
           <li>10,000+ practice questions</li>
         </ul>
+       
+        <Button className="w-full sm:w-40 bg-emerald-500 hover:bg-emerald-600 flex items-center justify-center order-last sm:order-none">
+          BUY NOW
+          <ArrowRight className="ml-2 h-4 w-4"/> 
+        </Button>
       </div>
-      <div className="flex-shrink-0">
+      <div className="flex-shrink-0 mb-4 sm:mb-0">
         <Image 
           src={SSCLogo} 
           alt="SSC Logo" 
           width={352}
           height={240}
-          className=" h-auto md:w-88 md:h-60"
+          className="w-64 h-auto sm:w-72 sm:h-auto md:w-88 md:h-60"
         />
+        
       </div>
     </div>
-  );
+  )
 }
