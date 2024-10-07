2a23c8482597dcc1c2c80355c0e9cf9f0cfb0fcc
sivakkumaran44 <sivakkumaran.g@sangaas.com>
2024-10-07 11:23:03 +0530

    screen

--- app/iframe/components/SuccessProbabilityChart.tsx
+++ app/iframe/components/SuccessProbabilityChart.tsx
@@ -1,37 +1,45 @@
-import React from 'react'
-import SuccessProbabilityTracker from './SuccessProbabilityTracker'
-import SuccessProbabilityProcessChart from './SuccessProbabilityProcessChart'
-import  TestInfoCard  from './TestInfoCard'
-import { RelativeOpportunitiesCard } from './RelativeOpportunitiesCard'
-import SuccessRate from './SuccessRate'
+import React from 'react';
+import SuccessProbabilityTracker from './SuccessProbabilityTracker';
+import SuccessProbabilityProcessChart from './SuccessProbabilityProcessChart';
+import TestInfoCard from './TestInfoCard';
+import { RelativeOpportunitiesCard } from './RelativeOpportunitiesCard';
+import SuccessRate from './SuccessRate';
+
 const SuccessProbabilityChart = () => {
   return (
     <div className="container mx-auto px-4">
-      <div className="hidden lg:flex h-14 mb-6 border border-slate-200 bg-slate-100 text-slate-700 text-xl rounded-lg justify-center items-center">
-        Exam Selection Probability
+      <div className="w-full">
+          <div className="h-[600px] w-[300px] overflow-hidden">
+            <SuccessRate />
       </div>
- <div>
-      <SuccessRate/>
-    </div>
+</div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         <div className="w-full">
-        <SuccessProbabilityProcessChart />    
+          <div className="h-[600px] w-[300px] overflow-hidden">
+            <SuccessProbabilityProcessChart />
+          </div>
         </div>
         <div className="w-full">
-   <SuccessProbabilityTracker />
+          <div className="h-[600px] w-[300px] overflow-hidden">
+            <SuccessProbabilityTracker />
+          </div>
         </div>
-        </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         <div className="w-full">
+          <div className="h-[600px] w-[300px] overflow-hidden">
             <TestInfoCard />
-      </div>
-      <div className="w-full ">
-        <RelativeOpportunitiesCard />
-      </div>
-   
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="h-[600px] w-[300px] overflow-hidden">
+            <RelativeOpportunitiesCard />
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SuccessProbabilityChart
+export default SuccessProbabilityChart;
