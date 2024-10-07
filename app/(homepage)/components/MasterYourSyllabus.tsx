8fcfb51da92cfdf05425302f6b1054c9231a73cb
sivakkumaran44 <sivakkumaran.g@sangaas.com>
2024-10-07 16:57:15 +0530

    screen

--- app/(homepage)/components/MasterYourSyllabus.tsx
+++ app/(homepage)/components/MasterYourSyllabus.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 const MasterYourSyllabus = () => {
   return (
-    <div className="flex flex-col-reverse md:flex-row items-center justify-between p-8 bg-white">
+    <div className="flex flex-col-reverse md:flex-row items-center justify-between p-8 mb-2 bg-white">
       <div className="w-full md:w-1/2 md:pl-8 mb-8 md:mb-0">
         <h3 className="scroll-m-20 text-2xl mb-6 text-emerald-500 font-semibold tracking-tight">
           Master Your Syllabus
@@ -12,7 +12,7 @@ const MasterYourSyllabus = () => {
           exam syllabus effectively.
         </p>
       </div>
-      <div className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] w-[300px] shadow-xl">
+      <div className="relative mx-auto border-gray-800 dark:border-gray-800 mb-6 bg-gray-800 border-[14px] rounded-[2.5rem] h-[600px] w-[300px] shadow-xl">
           <div className="w-[148px] h-[18px] bg-gray-800 top-0 rounded-b-[1rem] left-1/2 -translate-x-1/2 absolute"></div>
           <div className="h-[46px] w-[3px] bg-gray-800 absolute -left-[17px] top-[124px] rounded-l-lg"></div>
          <div className="h-[46px] w-[3px] bg-gray-800 absolute -left-[17px] top-[178px] rounded-l-lg"></div>
