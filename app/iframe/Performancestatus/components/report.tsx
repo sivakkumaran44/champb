7bee12ebc1e36b1d50f4d0c8b32ab6c4d02b9f0a
sivakkumaran44 <sivakkumaran.g@sangaas.com>
2024-10-07 16:34:16 +0530

    screen

--- app/iframe/Performancestatus/components/report.tsx
+++ app/iframe/Performancestatus/components/report.tsx
@@ -7,35 +7,36 @@ import Speed from './Speed';
 import ProgressTrackingChart from './processtracking';
 import ImprovementAreasChart from './areachart';
 import { TimeManagementChart } from './timemangementchart';
+
 const Report = () => {
   return (
-    <div className="relative w-full max-w-5xl ">
-          <div className="flex flex-col md:flex-row gap-4 mb-4">
+    <div className="relative w-full max-w-5xl overflow-x-hidden">
+      <div className="flex flex-col md:flex-row gap-4 mb-4">
         <div className="">
           <PieChartComponent />
         </div>
         <div className="p-4">
           <Barchart />  
-        </div> 
+        </div>
       </div>
-      <div className=" mb-4 p-4">
-  <ProgressTrackingChart />
-</div>
-<div className=" mb-4 p-4">
- <TimeManagementChart/>
-</div>
+      <div className="mb-4 p-4">
+        <ProgressTrackingChart />
+      </div>
+      <div className="mb-4 p-4">
+        <TimeManagementChart/>
+      </div>
       <div className="flex flex-col md:flex-row gap-4">
-        <div className=" p-4">
+        <div className="p-4">
           <AccuracyComponent />
         </div>
-        <div className=" p-4">
+        <div className="p-4">
           <Speed />  
-        </div> 
+        </div>
       </div>
-      <div className=" ">
-      <ImprovementAreasChart />
+      <div className="px-2 pb-4">
+        <ImprovementAreasChart />
+      </div>
     </div>
-          </div>
   );
 };
 
