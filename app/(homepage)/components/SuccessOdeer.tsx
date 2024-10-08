f0b9d4ec920f1e80b22d48b356d38b03702aa344
sivakkumaran44 <sivakkumaran.g@sangaas.com>
2024-10-08 13:55:28 +0530

    screen

--- app/(homepage)/components/SuccessOdeer.tsx
+++ app/(homepage)/components/SuccessOdeer.tsx
@@ -1,16 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
 export default function Component() {
   return (
     <div className="flex flex-col md:flex-row items-center justify-between p-8 bg-white relative">
       <div className="w-full md:w-1/2 mb-8 md:mb-0 relative z-10">
        <div className="relative mx-auto h-[608px] w-[308px] rounded-[2.5rem] border-[4px] bg-gradient-to-r from-blue-200 to-purple-300 border-gray-500">
-          <div className="relative mx-auto h-[600px] w-[300px] rounded-[2.2rem] border-[10px] border-transparent bg-slate-950 p-1 shadow-xl">
+          <div className="relative mx-auto h-[600px] w-[300px] rounded-[2.2rem] border-[6px] border-transparent bg-slate-950 p-1 shadow-xl">
             <div className="absolute left-1/2 top-0 h-[18px] w-[148px] -translate-x-1/2 rounded-b-[1rem] bg-black"></div>
             <div className="absolute left-1/2 top-0 h-[10px] w-[10px] -translate-x-1/2 rounded-b-[1rem] bg-black"></div>
-            <div className="absolute -left-[17px] top-[124px] h-[46px] w-[3px] rounded-l-lg bg-gradient-to-b from-slate-300 to-slate-600 border border-gray-500"></div>
-            <div className="absolute -left-[17px] top-[178px] h-[46px] w-[3px] rounded-l-lg bg-gradient-to-b from-slate-300 to-slate-600 border border-gray-500"></div>
-            <div className="absolute -right-[17px] top-[124px] h-[46px] w-[3px] rounded-r-lg bg-gradient-to-b from-slate-300 to-slate-600 border border-gray-500"></div>
-            <div className="absolute -right-[17px] top-[178px] h-[46px] w-[3px] rounded-r-lg bg-gradient-to-b from-slate-300 to-slate-600 border border-gray-500"></div>
+            <div className="absolute -left-[12px] top-[124px] h-[46px] w-[3px] rounded-l-lg bg-gradient-to-b from-slate-300 to-slate-600 border border-gray-500"></div>
+            <div className="absolute -left-[12px] top-[178px] h-[46px] w-[3px] rounded-l-lg bg-gradient-to-b from-slate-300 to-slate-600 border border-gray-500"></div>
+            <div className="absolute -right-[12px] top-[124px] h-[46px] w-[3px] rounded-r-lg bg-gradient-to-b from-slate-300 to-slate-600 border border-gray-500"></div>
+            <div className="absolute -right-[12px] top-[178px] h-[46px] w-[3px] rounded-r-lg bg-gradient-to-b from-slate-300 to-slate-600 border border-gray-500"></div>
             <div className="h-full w-full overflow-hidden rounded-[2rem] bg-white">
               <iframe
               src="https://champb.vercel.app/iframe"
