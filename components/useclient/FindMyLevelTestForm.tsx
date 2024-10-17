67651c9ab4777d9cec023b5fcbf0463580e059dd
sivakkumaran44 <sivakkumaran.g@sangaas.com>
2024-10-17 13:00:40 +0530

    screen

--- components/useclient/FindMyLevelTestForm.tsx
+++ components/useclient/FindMyLevelTestForm.tsx
@@ -3,29 +3,15 @@ import React, { useState } from 'react';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import OtpVerification from '@/components/Auth/OtpScreen/OtpScreen';
+
 
 const FindMyLevelTestForm = () => {
-  const [showOtpVerification, setShowOtpVerification] = useState(false);
-  const [countryCode, setCountryCode] = useState('+91');
-  const [mobileNumber, setMobileNumber] = useState('');
+ const [mobileNumber, setMobileNumber] = useState('');
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    setShowOtpVerification(true);
-  };
-
-  const handleOtpVerificationClose = () => {
-    setShowOtpVerification(false);
-  };
-
-  const handleUpdateMobileNumber = (newMobileNumber: string) => {
-    setMobileNumber(newMobileNumber);
-  };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form >
         <div className="flex justify-center items-center px-4 sm:px-6 md:px-8 lg:px-10 h-[calc(100%-6rem)] sm:h-[calc(100%-7rem)] md:h-[calc(100%-9rem)] lg:h-[calc(100%-11rem)]">
           <div className="w-84 max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-sm space-y-4 sm:space-y-6 md:space-y-8">
             <Select name="exam-select">
@@ -60,7 +46,7 @@ const FindMyLevelTestForm = () => {
               <Select 
                 name="country-code-select" 
                 defaultValue="+91"
-                onValueChange={(value) => setCountryCode(value)}
+               
               >
                 <SelectTrigger className="w-1/4 sm:w-1/5 md:w-1/6 bg-white text-slate-900 border-none rounded-md shadow-sm h-12 sm:h-14">
                   <SelectValue placeholder="+91" />
