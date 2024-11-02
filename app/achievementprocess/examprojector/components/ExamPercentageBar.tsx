a8ab03828333756251c24fdf3e623d1f238c551a
sivakkumaran44 <sivakkumaran.g@sangaas.com>
2024-11-02 13:11:42 +0530

    screen

--- app/achievementprocess/examprojector/components/ExamPercentageBar.tsx
+++ app/achievementprocess/examprojector/components/ExamPercentageBar.tsx
@@ -208,7 +208,7 @@ export default function Component({ value = data.additionalData.progress }: Prog
       </div>
       <div className="flex items-center justify-between ">
         <p className="text-sm sm:text-lg font-medium flex-grow">{progressMessage}</p>
-          <Clock className="w-5 h-5 mr-2 text-blue-500" />
+          <Clock className="w-5 h-5  text-blue-500" />
           <span className="text-sm sm:text-base font-medium">
             {data.additionalData.daysLeft} days left
           </span>
